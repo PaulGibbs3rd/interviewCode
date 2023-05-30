842bcc67a467e5ab590804fd4efda7244b6e96be
Paul Gibbs <123662740+PaulGibbs3rd@users.noreply.github.com>
2023-05-30 16:39:56 -0700

    Add files via upload

---
+++ binary search tree extended/main.cpp
@@ -0,0 +1,33 @@
+#include "bst.cpp"
+
+int main()
+{
+	BinarySearchTree<int> bst;
+	int x = 0;
+	
+	cout << "insert: " << endl;
+	cin >> x;
+	while (x != 0)
+	{
+		bst.insert(x);
+		cin >> x;
+	}
+	
+	bst.printBST();
+
+	int k1 = 0, k2 = 0;
+	cout << "Please enter the range: ";
+	cin >> k1 >> k2;
+	bst.printRange(k1, k2);
+	
+	cout << "remove: " << endl;
+	cin >> x;
+	while (x != 0)
+	{
+		bst.remove(x);
+		cin >> x;
+	}	
+	
+	bst.printBST();
+
+}
