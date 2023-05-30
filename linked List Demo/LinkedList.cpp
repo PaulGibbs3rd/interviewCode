1da465c4da0d378e23465368f4a4eb6cf0f5caa8
Paul Gibbs <123662740+PaulGibbs3rd@users.noreply.github.com>
2023-05-30 16:31:15 -0700

    Add files via upload

---
+++ linked List Demo/LinkedList.cpp
@@ -0,0 +1,110 @@
+#ifndef LL
+#define LL
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class LinkedList 
+{
+ 
+  public:
+  // Default constructor
+  LinkedList(): size(0)
+  {
+	head = new NodeType();
+  }
+  
+  //Destructor
+  ~LinkedList()
+  {
+	clear();
+  }
+
+  bool empty( ) const
+  {
+	return get_size() == 0; 
+  } 
+  
+  // Returns the list to the empty state.  
+  void clear()
+  {
+	while (!empty())
+	{
+		pop_front();
+	}
+  }
+
+  // Returns the number of items in the list.
+  int get_size() const
+  {
+	return size;
+  }
+
+  // Checks if item is in the list. 
+  bool find(const T& item) const
+  {
+	NodeType* ptr = head->next;
+	while (ptr->data != item && ptr != nullptr)
+	{
+		ptr = ptr->next;
+	}
+	if (ptr->data == item)
+		return true;
+	else
+		return false;  	
+  }
+
+  // Inserts item at the front.
+  void push_front(const T& item)
+  {
+	NodeType* ptr = new NodeType(item);
+
+	ptr->next = head->next;
+	head->next = ptr;	
+	++size;   	
+  }
+  
+  // Removes the first item.
+  void pop_front()
+  {
+	if (head->next != nullptr)
+	{
+		NodeType* ptr = head->next;
+		head->next = ptr->next;
+		delete ptr;
+		--size;
+	}
+	
+  }
+
+  // Prints the list.
+  void print() const
+  {
+	NodeType* ptr = head->next;
+	while (ptr != nullptr)
+	{
+		cout << ptr->data <<" | ";
+		ptr = ptr->next;
+	}  
+        cout << endl;
+ 	
+  }
+  
+  private:
+  struct NodeType
+  {
+	  T data;
+	  NodeType* next;
+	  
+	  NodeType(): data(), next(nullptr)
+	  {}
+
+	  NodeType(const T& d): data(d), next(nullptr)
+	  {}
+  };
+  int size;	// the size of the linked list.
+  NodeType* head;	// points to the header node.
+};
+
+#endif
+
---
+++ linked List Demo/main.cpp
@@ -0,0 +1,90 @@
+#include "Employee.cpp"
+#include "LinkedList.cpp"
+
+int main()
+{
+	LinkedList<int> intll;
+	int x = 0;
+	cout << "Create a list of integers: ";
+	cin >> x;
+	while (x != -1)
+	{
+		intll.push_front(x);
+		cin >> x;
+	}
+		
+        cout << "The size of the int linked list is " << intll.get_size() << endl;
+	cout << "The linked list is: ";
+	intll.print();
+
+	cout << "Enter the key: ";
+	cin >> x;
+	cout << "Is "<< x <<" in the list? " << intll.find(x) << endl;	
+	
+	cout << "How many integers you want to remove? ";
+	cin >> x;	
+	for (int i = 0; i < x; i++)
+		intll.pop_front();
+
+	cout << "The linked list is: ";
+	intll.print();
+	cout << endl;
+
+	
+	LinkedList<string> strll;
+	string s;
+	cout << "Create a list of strings (stop adding when entering \"exit\" ): ";
+	cin >> s;
+	while (s != "exit")
+	{
+		strll.push_front(s);
+		cin >> s;
+	}
+
+        cout << "The size of the linked list is " << strll.get_size() << endl;
+	cout << "The linked list is: ";
+	strll.print();
+	cout << endl;
+	
+	int k = 0;
+	cout << "enter the number of elements to be removed from the string linked list: ";
+	cin >> k;
+	for (int i = 0; i < k; i++)
+	{
+		strll.pop_front();
+	}
+	cout << "The linked list is: ";
+	strll.print();
+	cout << endl;
+
+	LinkedList<Employee> emplll;
+	string name, dept;
+	int id = 0;
+	cout << "Create a list of employees: " << endl;
+	cout << "Input name, id, and department name (stop adding when entering id 0): ";
+	cin >> name >> id >> dept;
+	while (id != 0 )
+	{
+		Employee e(name, id, dept);
+		emplll.push_front(e);
+		cout << "Input name, id, and department name (stop adding when entering id 0): ";
+		cin >> name >> id >> dept;
+	}
+        cout << "The size of the employee linked list is " << emplll.get_size() << endl;
+
+	cout << "The linked list is: ";
+	emplll.print();
+	cout << endl;
+	
+	k = 0;
+	cout << "enter the number of elements to be removed from the linked list: ";
+	cin >> k;
+	for (int i = 0; i < k; i++)
+	{
+		emplll.pop_front();
+	}
+	cout << "The linked list is: ";
+	emplll.print();
+	cout << endl;
+
+}
