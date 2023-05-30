b551c6821ed4e4670787179bb8f9ac3abd965936
Paul Gibbs <123662740+PaulGibbs3rd@users.noreply.github.com>
2023-05-30 16:49:07 -0700

    Add files via upload

---
+++ merge sort demo/main.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+#include <iostream>
+#include "sorting.cpp"
+#include "MergeSort.cpp"
+#include "bst.cpp"
+#include <fstream>
+
+using namespace std;
+
+template <typename C>
+void print(vector<C> v)
+{
+	for(int i = 0; i < v.size(); i++)
+		cout << v[i] << " ";
+	cout << endl;
+}
+
+int main()
+{
+
+
+	vector<int> v;
+	BinarySearchTree<int> bst;
+
+	ifstream input("sort.txt");
+	int number;
+	while (input >> number)
+	{	
+   		v.push_back(number);
+   		bst.insert(number);
+	}
+	input.close();
+	 
+	mergeSort(v);
+	print(v); 
+	
+	bst.printBST();
+
+	return 0;
+}
