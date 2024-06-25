ed659e39ec5e129487f04b393aef17c14f0dc208
Paul Gibbs <123662740+PaulGibbs3rd@users.noreply.github.com>
2024-06-25 12:42:19 -0700

    linkedlist implementation in c#
    Created a Linked List data structure with add display remove find and length functions

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListNamespace
+{
+    public class LinkedList
+    {
+        private Node head;
+
+        public LinkedList()
+        {
+            this.head = null;
+        }
+
+        public void Add(int data)
+        {
+            Node newNode = new Node(data);
+            if (head == null)
+            {
+                head = newNode;
+
+            }
+            else
+            {
+                Node current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+
+        public bool Remove(int data)
+        {
+            if (head == null)
+            {
+                return false;
+            }
+            if (head.Data == data)
+            {
+                head = head.Next;
+                return true;
+            }
+
+            Node current = head;
+            while (current.Next != null)
+            {
+                if (current.Next.Data == data)
+                {
+                    current.Next = current.Next.Next;
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public void Display()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.Write(current.Data + " -> ");
+                current = current.Next;
+            }
+            Console.WriteLine("null");
+        }
+
+        public bool Find(int data)
+        {
+            Node current = head;
+            while (current != null)
+            {
+              if (current.Data == data)
+              {
+                    return true;
+              }
+              current = current.Next;
+                
+                
+            }
+            return false;
+        }
+
+        public int Length()
+        {
+            int length = 0;
+            Node current = head;
+            while (current != null)
+            {
+                length++;
+                current = current.Next;
+            }
+            return length; 
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LinkedListNamespace;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LinkedList list = new LinkedList();
+
+        list.Add(0);
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+
+        list.Display();
+
+        list.Remove(2);
+        list.Display();
+        bool found = list.Find(0);
+        int lenngth = list.Length();
+
+        Console.WriteLine("found 0:" + found + " length: " + lenngth);
+    }
+}
